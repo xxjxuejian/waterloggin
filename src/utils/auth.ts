4cec4fa667c2ff74ad19db297575a2e640569e5c
xu xuejian <2498492824@qq.com>
2025-04-23 18:33:45 +0800

    fix: :bug: 对token存取做类型检查

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,46 +2,41 @@
 const TOKEN_INFO_KEY = "tokenInfo";
 const TOKEN_VALID_TIME = 1000 * 60 * 60 * 2; // 2小时
 
-// 设置token
+// 保存tokenInfo
 export const setToken = (token: string) => {
-  const tokenInfo = {
+  const tokenInfo: TokenInfo = {
     token,
     timestamp: Date.now(), // 记录获取token的时间戳
   };
   localStorage.setItem(TOKEN_INFO_KEY, JSON.stringify(tokenInfo));
 };
 
-// 获取token
-export const getToken = () => {
-  const tokenInfoStr = localStorage.getItem(TOKEN_INFO_KEY);
-  if (!tokenInfoStr) return null;
-
+// 获取完整 tokenInfo 的方法
+export const getTokenInfo = (): TokenInfo | null => {
+  const str = localStorage.getItem(TOKEN_INFO_KEY);
+  if (!str) return null;
   try {
-    const tokenInfo = JSON.parse(tokenInfoStr);
-    return tokenInfo.token;
+    const data = JSON.parse(str);
+    return isValidTokenInfo(data) ? data : null;
   } catch (err) {
-    console.error("获取token失败:", err);
+    console.error("获取tokenInfo失败:", err);
     return null;
   }
 };
 
+// 获取token
+export const getToken = (): string | null => {
+  const tokenInfo = getTokenInfo();
+  return tokenInfo?.token || null;
+};
+
 // 检查token是否过期
-export const isTokenExpired = () => {
+export const isTokenExpired = (): boolean => {
   // 获取不到token，就认为是过期了
-  const tokenInfoStr = localStorage.getItem(TOKEN_INFO_KEY);
-  if (!tokenInfoStr) return true;
+  const tokenInfo = getTokenInfo();
+  if (!tokenInfo) return true;
 
-  try {
-    const tokenInfo = JSON.parse(tokenInfoStr);
-    const currentTime = Date.now();
-    const tokenTime = tokenInfo.timestamp;
-
-    // 间隔大于有效期，就是过期了
-    return currentTime - tokenTime > TOKEN_VALID_TIME;
-  } catch (err) {
-    console.error("检查token是否过期失败:", err);
-    return true;
-  }
+  return Date.now() - tokenInfo.timestamp > TOKEN_VALID_TIME;
 };
 
 // 删除token
@@ -49,19 +44,12 @@ export const clearToken = () => {
   localStorage.removeItem(TOKEN_INFO_KEY);
 };
 
-// 获取完整 tokenInfo 的方法
-export const getTokenInfo = () => {
-  const str = localStorage.getItem(TOKEN_INFO_KEY);
-  if (!str) return null;
-  try {
-    return JSON.parse(str);
-  } catch (err) {
-    console.error("获取tokenInfo失败:", err);
-    return null;
-  }
-};
+interface TokenInfo {
+  token: string;
+  timestamp: number;
+}
 
-// interface TokenInfo {
-//   token: string;
-//   timestamp: number;
-// }
+// 类型守卫（防御性编程）
+function isValidTokenInfo(obj: any): obj is TokenInfo {
+  return obj && typeof obj.token === "string" && typeof obj.timestamp === "number";
+}
